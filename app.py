f42e1f9e9f98140039e2c896fed8fc316fa721b9
Georges <92797658+georgrmat@users.noreply.github.com>
2023-11-27 21:10:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ st.write(
     "Assess the potential long-term profitability of your trading strategy."
 )
 
-money = st.number_input("Initial capital of investment", min_value=0, max_value=1000000, value=100)
-N = st.slider("Number of trades", min_value=0, max_value=500, value=100)
-win_rate = st.slider("Win rate (percentage of trades that hit take profit)", min_value=0, max_value=100, value=50)
-risk = st.slider("**Risk per trade** (percentage of your account that you are prepared to allocate for risk in each trade)", min_value=0, max_value=100, value=1)
-reward = st.slider("**Profit per trade** (targeted percentage of your account that you aim to achieve as profit)", min_value=0, max_value=100, value=3)
+money = st.number_input("**Initial capital of investment**", min_value=0, max_value=1000000, value=100)
+N = st.slider("**Number of trades**", min_value=0, max_value=500, value=100)
+win_rate = st.slider("**Win rate**: percentage of trades that hit take profit", min_value=0, max_value=100, value=50)
+risk = st.slider("**Risk per trade**: percentage of your account that you are prepared to allocate for risk in each trade (for a long term strategy and if you're a beginner, don't risk more that 1% per trade)", min_value=0, max_value=100, value=1)
+reward = st.slider("**Profit per trade**: targeted percentage of your account that you aim to achieve as profit (an excessively ambitious target is less likely to be consistently achieved)", min_value=0, max_value=100, value=3)
 
 trades = []
 for i in range(N):
