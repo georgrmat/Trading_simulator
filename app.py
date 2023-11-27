e18a6ca11002f6158e3831097c03874f8326d630
Georges <92797658+georgrmat@users.noreply.github.com>
2023-11-27 20:47:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from random import random
 import streamlit as st
 
 money = st.number_input("Enter your initial amount of money", min_value=0, max_value=1000000, value=25)
-N = st.slider("Select the number of trades", min_value=0, max_value=100000, value=100)
+N = st.slider("Select the number of trades", min_value=0, max_value=1000, value=100)
 win_rate = st.slider("Select your percentage of wins (of taking profit)", min_value=0, max_value=100, value=50)
 risk = st.slider("Select the percentage of you account you're willing to risk at each trade", min_value=0, max_value=100, value=50)
 reward = st.slider("Select the percentage of profit at which you close your trades", min_value=0, max_value=100, value=50)
@@ -12,10 +12,10 @@ reward = st.slider("Select the percentage of profit at which you close your trad
 trades = []
 for i in range(N):
     if random() < win_rate:
-        money *= 1 + reward
+        money *= 1 + reward/100
         print(i, "  ", "Take Profit", "  ", np.round(money, 2))
     else:
-        money *= 1 - risk
+        money *= 1 - risk/100
         print(i, "  ", "Stop Loss", "  ", np.round(money, 2))
     trades.append(money)
 
