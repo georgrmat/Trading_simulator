3c7b0b0c4fb25f9775a9baf8c4c489a53160cc36
Georges <92797658+georgrmat@users.noreply.github.com>
2023-11-27 20:49:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import streamlit as st
 money = st.number_input("Enter your initial amount of money", min_value=0, max_value=1000000, value=25)
 N = st.slider("Select the number of trades", min_value=0, max_value=1000, value=100)
 win_rate = st.slider("Select your percentage of wins (of taking profit)", min_value=0, max_value=100, value=50)
-risk = st.slider("Select the percentage of you account you're willing to risk at each trade", min_value=0, max_value=100, value=50)
-reward = st.slider("Select the percentage of profit at which you close your trades", min_value=0, max_value=100, value=50)
+risk = st.slider("Select the percentage of you account you're willing to risk at each trade", min_value=0, max_value=100, value=1)
+reward = st.slider("Select the percentage of profit at which you close your trades", min_value=0, max_value=100, value=3)
 
 trades = []
 for i in range(N):
